4d26d839865ce5e5d800a9c71c8e57e2b5d749d3
“Naeem <sarimali245678@gmail.com>
2024-09-30 15:32:09 +0500

    assignment-2

---
+++ src/components/profile.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+
+export default function Profile(){
+    return(
+        <div>
+             <div className="img1">
+            <img src="https://png.pngtree.com/png-vector/20240309/ourmid/pngtree-developers-are-coding-programs-on-computers-programmers-are-analyzing-data-png-image_11902650.png" alt="" />
+        </div>
+        <div className="bg-slate-500 h-screen  pt-10 pl-10 text-6xl">
+            <h1><span>Hi, </span><span className="text-6xl  text-emerald-500"><br/><br/>I am</span><br/><br/><span className="naeem">Naeem Ahmed</span> </h1><br/>
+            <p><span className="graphic">Graphic Designer and</span><br/><br/><span className="web">Web Developer</span> </p>
+            
+        </div>
+       
+        
+       
+       
+        </div>
+    )
+}
